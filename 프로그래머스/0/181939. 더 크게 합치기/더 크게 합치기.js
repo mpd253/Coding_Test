3a2edb1e5870ac46cf5cab160571b93d6eff4215
ChoiYoungKyu <82489360+mpd253@users.noreply.github.com>
2024-05-27 17:50:41 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,6 @@
+function solution(a, b) {
+    let strA = String(a);
+    let strB = String(b);
+    
+    return Number(strA + strB) > Number(strB + strA) ? Number(strA + strB) : Number(strB + strA);
+}
