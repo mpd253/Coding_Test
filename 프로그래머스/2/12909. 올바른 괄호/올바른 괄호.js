8b0bc8f6e96d009c72b1333bcb158a6e79ebf31e
ChoiYoungKyu <82489360+mpd253@users.noreply.github.com>
2024-08-12 16:32:43 +0900

    [level 2] Title: 올바른 괄호, Time: 2.60 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,11 @@
+function solution(s){
+    let count = 0;
+
+    for (let i=0; i<s.length; i++) {
+        s[i] === "(" ? count += 1 : count += -1;
+        if (count < 0) {
+            break;
+        }
+    }
+    return count === 0 ? true : false;
+}
