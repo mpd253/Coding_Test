848aeef87f062e6437e460f9929169bc5f3ccdb9
ChoiYoungKyu <82489360+mpd253@users.noreply.github.com>
2024-08-19 17:23:51 +0900

    [level 2] Title: 숫자의 표현, Time: 2.30 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    var answer = 0;
+    
+    for(let i=1; i<=n; i++) {
+        let sum = 0;
+        for(let j=i; j<=n; j++) {
+            sum += j;
+            if(sum === n) {
+                answer++;
+                break;
+            }
+            if(sum > n) {
+                break;
+            }
+        }
+    }
+    return answer;
+}
