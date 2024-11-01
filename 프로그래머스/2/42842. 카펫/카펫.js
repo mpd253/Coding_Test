f490880439d05f90d739197d94ddb05309b73903
ChoiYoungKyu <82489360+mpd253@users.noreply.github.com>
2024-11-01 17:22:57 +0900

    [level 2] Title: 카펫, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,13 @@
+function solution(brown, yellow) {
+    let sum = brown + yellow;
+    
+    for(let height = 3; height <= brown; height++) {
+        if(sum % height === 0) {
+            let width = sum / height;
+            
+            if((width - 2) * (height - 2) == yellow) {
+                return [width, height];
+            }
+        }
+    }
+}
