508fadb9504b06b50ac5638a5fff4f315f718c2f
ChoiYoungKyu <82489360+mpd253@users.noreply.github.com>
2024-11-29 17:41:17 +0900

    [level 1] Title: 기사단원의 무기, Time: 93.16 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,36 +1,15 @@
 function solution(number, limit, power) {
     const attackPoint = new Array(number).fill(0);
-    // 33점, 37점
-    // const attackPoint = [];
     
     for(let i=1; i<=number; i++) {
-        for(let j=i; j<=number; j+=i) {
-            attackPoint[j - 1]++;
+        for(let j=1; j<=Math.sqrt(i); j++) {
+            if(i % j === 0) {
+                attackPoint[i-1]++;
+                if(j !== i / j) {
+                    attackPoint[i-1]++;
+                }
+            }
         }
-        // let count = 0;
-        
-        // 33점
-        // for(let j=1; j<=i; j++) {
-        //     if(i % j === 0) {
-        //         count++;
-        //     }
-        // }
-        
-        // 37점
-        // for(let j=1; j<=Math.sqrt(i); j++) {
-        //     if(i % j === 0) {
-        //         count++;
-        //         if(j !== i / j) {
-        //             count++;
-        //         }
-        //     }
-        // }
-        
-        // if(count > limit) {
-        //     attackPoint.push(2);
-        // } else {
-        //     attackPoint.push(count);        
-        // }
     }
     
     for(let i=0; i<number; i++) {
