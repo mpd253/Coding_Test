117f793d26481be7c8122859ce0bcffcaa69189e
youngkyu <82489360+mpd253@users.noreply.github.com>
2025-02-12 20:55:16 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 33.70 ms, Memory: 60.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,25 @@
+function solution(queue1, queue2) {
+    let sum1 = queue1.reduce((acc, cur) => acc + cur, 0);
+    let sum2 = queue2.reduce((acc, cur) => acc + cur, 0);
+    let target = (sum1 + sum2) / 2;
+
+    let newQueue = [...queue1, ...queue2];
+    
+    let left = 0;
+    let right = queue1.length;
+    let count = 0;
+    
+    while (left < newQueue.length && right < newQueue.length * 2) {
+        if (sum1 === target) return count;
+
+        if (sum1 > target) {
+            sum1 -= newQueue[left];
+            left++;
+        } else {
+            sum1 += newQueue[right];
+            right++;
+        }
+        count++;
+    }
+    return -1;
+}
