039bfde4f30c97f7e927b494f8b2c2a0fa042f94
youngkyu <82489360+mpd253@users.noreply.github.com>
2025-04-02 16:28:45 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    let answer = "";
+    const vowels = ["a", "e", "i", "o", "u"];
+    
+    for(let i=0; i<my_string.length; i++) {
+        if(!vowels.includes(my_string[i])) {
+            answer += my_string[i];
+        }
+    }
+    
+    return answer;
+}
