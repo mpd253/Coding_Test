88d7f101b1b9229e94966db9aeba33ad4c6d523d
youngkyu <82489360+mpd253@users.noreply.github.com>
2025-05-08 17:01:39 +0900

    [level 1] Title: 행렬의 덧셈, Time: 29.90 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,16 @@
+function solution(arr1, arr2) {
+    const rows = arr1.length;
+    const cols = arr1[0].length;
+    
+    const result = [];
+    
+    for(let i=0; i<rows; i++) {
+        const row = [];
+        for (let j=0; j<cols; j++) {
+            row.push(arr1[i][j] + arr2[i][j]);
+        }
+        result.push(row);
+    }
+    
+    return result;
+}
